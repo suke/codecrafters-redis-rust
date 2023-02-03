00759c5e3fb9886a2a499b690d2e36eb87807ba0
suke <suke083.tech@gmail.com>
2023-02-03 23:12:52 +0900

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn reply_pong(mut stream: TcpStream) {
+    let response = "+PONG\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                reply_pong(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
