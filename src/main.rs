588077690fb90b3e5e9bdd8a82a1f263ded0877f
suke <suke083.tech@gmail.com>
2023-02-03 23:26:17 +0900

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
-fn reply_pong(mut stream: TcpStream) {
-    let response = "+PONG\r\n";
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-}
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                reply_pong(stream);
+                handle_request(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_request(mut stream: TcpStream) {
+    loop {
+        let mut buffer = [0; 1028];
+        stream
+            .read(&mut buffer)
+            .expect("error while reading from connection");
+
+        if buffer.len() == 0 {
+            break;
+        }
+
+        let response = "+PONG\r\n";
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().expect("error while writing to connection");
     }
 }
